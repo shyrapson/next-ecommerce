8fa0385d355c30470d5eee296e6ddcd9a772f300
shyrapson <meetshyrapson@gmail.com>
2023-03-08 10:40:14 +0100

    register and signup page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import Navbar from './Navbar';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({ title, children }) {
   return (
